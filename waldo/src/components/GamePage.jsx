2d45f953f875fca1d6cd47f2b972100fedbaae87
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-03-05 13:17:51 -0500

    Set up picture to have fixed coordinates

--- waldo/src/components/GamePage.jsx
+++ waldo/src/components/GamePage.jsx
@@ -1,9 +1,19 @@
 import '../styles/GamePage.css'
 
 function GamePage(){
+    function handleClick(event){
+        const x = event.nativeEvent.offsetX;
+        const y = event.nativeEvent.offsetY;
+
+        console.log(`x: ${x} and y: ${y}`);
+    }
+
     return (
         <>
-            <h1 className='title'>Find Waldo</h1>
+            <h1 className='title' style={{width: '200px'}}>Find Waldo</h1>
+            <div className='main-content' onClick={handleClick}>
+
+            </div>
         </>
     )
 }
