fbdc87e736472e1ea59febea52e144bb7de51273
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-03-05 06:20:34 -0500

    Added landing page with background picture

---
+++ waldo/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='appBackground'>
+      <button className='playBtn'>Star Game</button>
+    </div>
+  )
+}
+
+export default App
