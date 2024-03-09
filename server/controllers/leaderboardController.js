fb668cea9dfcefeb67c252656a86e9ce0e59496e
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-03-09 05:51:41 -0500

    Updated leaderboard to make user query and display all users

--- server/controllers/leaderboardController.js
+++ server/controllers/leaderboardController.js
@@ -3,7 +3,7 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/users');
 
 exports.allUsers = asyncHandler(async(req, res, next) => {
-    const users = await User.find();
+    const users = await User.find().sort({time: 1});
 
     res.json(users);
 })
--- waldo/src/components/LeaderboardPage.jsx
+++ waldo/src/components/LeaderboardPage.jsx
@@ -1,16 +1,31 @@
 import { useState, useEffect } from 'react';
 
 function LeaderboardPage() {
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:3000/leaderboard')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
+            .then(response => response.json())
+            .then(data => {
+                setUsers(data);
+            }).catch(error => {
+                console.error('Error fetching leaderboard', error);
+            })
     }, []);
-    const [users, setUsers] = useState([]);
-    return (<p>Leaderboard</p>);
+
+
+    return (
+        <div>
+            <h1>Leaderboard</h1>
+            <ul>
+                {users.map(user=>(
+                    <li key={user._id}>
+                        {user.username}: {user.time}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default LeaderboardPage;
